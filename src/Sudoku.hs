f3e04f05efcf99ebb06a02c543abafa0dd67370b
Vinay Nagaraj <bnvinay92@gmail.com>
2018-01-24 17:26:24 +0530

    Solve sudoku

--- src/Sudoku.hs
+++ src/Sudoku.hs
@@ -1,7 +1,7 @@
 module Sudoku where
 
 import           Data.Char  (digitToInt)
-import           Data.List  (intercalate)
+import           Data.List  (intercalate, sort, sortOn)
 import           Data.Map   (Map, fromList, insert, lookup, toAscList)
 import           Data.Maybe (fromJust)
 
@@ -13,38 +13,71 @@ data Cell =
 instance Ord Cell where
   compare (Cell a b) (Cell x y) = compare (a * 9 + b) (x * 9 + y)
 
-data Digit
-  = Fixed Int
-  | OneOf [Int]
-  deriving (Eq, Show)
-
-type Space = [Cell]
+type Digit = [Int]
 
 type Board = Map Cell Digit
 
-testBoard = readBoard "003020600900305001001806400008102900700000008006708200002609500800203009005010300"
+testBoard = readBoard ".....6....59.....82....8....45........3........6..3.54...325..6.................."
 
-row :: Int -> Space
-row x = [Cell x y | y <- [0 .. 8]]
+easyBoard = readBoard "003020600900305001001806400008102900700000008006708200002609500800203009005010300"
 
-find :: Board -> Cell -> Digit
-find board cell = fromJust $ Data.Map.lookup cell board
+rows = [[Cell x y | y <- [0 .. 8]] | x <- [0 .. 8]]
 
-readBoard :: String -> Board
-readBoard str = fromList $ zip (concatMap row [0 .. 8]) [toDigit s | s <- str]
+cols = [[Cell x y | x <- [0 .. 8]] | y <- [0 .. 8]]
+
+grids = map grid origins
   where
-    toDigit '0' = OneOf [1 .. 9]
-    toDigit c   = Fixed $ digitToInt c
+    origins = [Cell x y | x <- [0, 3, 6], y <- [0, 3, 6]]
+    grid (Cell x y) = [Cell a b | a <- [x .. x + 2], b <- [y .. y + 2]]
+
+readBoard str = fromList $ zip (concat rows) [toDigit s | s <- str]
+  where
+    toDigit c
+      | c == '0' || c == '.' = [1 .. 9]
+      | otherwise = [digitToInt c]
 
 ppBoard board = unlines $ intercalate [line] sections
   where
+    ppDigit [d] = show d
+    ppDigit _   = "."
     sections = map (map (unwords . intercalate ["|"])) digitsChunked
-    digitsChunked = (splitEvery 3 . splitEvery 3 . splitEvery 3) digitList
-    digitList = map (ppDigit . snd) $ toAscList board
-    ppDigit (Fixed d) = show d
-    ppDigit _         = "."
+      where
+        digitsChunked = (splitEvery 3 . splitEvery 3 . splitEvery 3) digitList
+        digitList = map (ppDigit . snd) $ toAscList board
     line = "- - - + - - - + - - -"
     splitEvery n [] = []
     splitEvery n lst =
       let (head, tail) = splitAt n lst
       in head : splitEvery n tail
+
+solve board =
+  case solve' board of
+    (Just b) -> putStrLn $ ppBoard b
+    _        -> putStrLn "No solution"
+
+solve' board =
+  let complete = isFilled board
+      valid = isValid board
+  in case (complete, valid) of
+       (True, True) -> Just board
+       (_, True) -> firstJust guessList (\(c, d) -> solve' $ insert c [d] board)
+       _ -> Nothing
+  where
+    guessList =
+      concatMap (\(cell, digits) -> [(cell, x) | x <- digits]) $
+      sortOn (length . snd) $ filter (\d -> length (snd d) > 1) $ toAscList board
+    firstJust [] f = Nothing
+    firstJust (x:xs) f =
+      let r = f x
+      in case r of
+           j@(Just _) -> j
+           _          -> firstJust xs f
+
+isValid board = all (isValidSpace . map (find board)) (rows ++ cols ++ grids)
+  where
+    find board cell = fromJust $ Data.Map.lookup cell board
+    isValidSpace :: [Digit] -> Bool
+    isValidSpace = comparePairwise . sort . filter (\d -> length d == 1)
+    comparePairwise xs = and $ zipWith (/=) xs (drop 1 xs)
+
+isFilled board = all (\lst -> length lst == 1) $ map snd $ toAscList board
