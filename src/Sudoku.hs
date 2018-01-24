7e9ac0070e4b5cd7f08f87a166ac79ca3157be3e
Vinay Nagaraj <bnvinay92@gmail.com>
2018-01-24 18:35:23 +0530

    Add pruning

--- src/Sudoku.hs
+++ src/Sudoku.hs
@@ -5,21 +5,16 @@ import           Data.List  (intercalate, sort, sortOn)
 import           Data.Map   (Map, fromList, insert, lookup, toAscList)
 import           Data.Maybe (fromJust)
 
-data Cell =
-  Cell Int
-       Int
-  deriving (Eq, Show)
-
-instance Ord Cell where
-  compare (Cell a b) (Cell x y) = compare (a * 9 + b) (x * 9 + y)
+data Cell = Cell Int Int deriving (Eq, Show)
+instance Ord Cell where compare (Cell a b) (Cell x y) = compare (a * 9 + b) (x * 9 + y)
 
 type Digit = [Int]
-
 type Board = Map Cell Digit
 
-testBoard = readBoard ".....6....59.....82....8....45........3........6..3.54...325..6.................."
-
+hardBoard = readBoard ".....6....59.....82....8....45........3........6..3.54...325..6.................."
 easyBoard = readBoard "003020600900305001001806400008102900700000008006708200002609500800203009005010300"
+cardBoard = readBoard "200080300060070084030500209000105408000000000402706000301007040720040060004010003"
+ezBoard = readBoard "000003017015009008060000000100007000009000200000500004000000020500600340340200000"
 
 rows = [[Cell x y | y <- [0 .. 8]] | x <- [0 .. 8]]
 
@@ -29,6 +24,8 @@ grids = map grid origins
   where
     origins = [Cell x y | x <- [0, 3, 6], y <- [0, 3, 6]]
     grid (Cell x y) = [Cell a b | a <- [x .. x + 2], b <- [y .. y + 2]]
+
+find board cell = fromJust $ Data.Map.lookup cell board
 
 readBoard str = fromList $ zip (concat rows) [toDigit s | s <- str]
   where
@@ -50,19 +47,34 @@ ppBoard board = unlines $ intercalate [line] sections
       let (head, tail) = splitAt n lst
       in head : splitEvery n tail
 
+prune board =
+  if pruned == board
+    then pruned
+    else prune pruned
+  where
+    pruned = foldl prune' board $ rows ++ cols ++ grids
+    prune' :: Board -> [Cell] -> Board
+    prune' board space = foldl (\b (c, ds) -> insert c ds b) board $ zip space $ map (remove fixList) digits
+      where
+        fixList = concat $ filter (\ds -> length ds == 1) digits
+        digits = map (find board) space
+        remove _ [x]   = [x]
+        remove set lst = filter (`notElem` set) lst
+
 solve board =
   case solve' board of
     (Just b) -> putStrLn $ ppBoard b
     _        -> putStrLn "No solution"
 
-solve' board =
-  let complete = isFilled board
-      valid = isValid board
-  in case (complete, valid) of
-       (True, True) -> Just board
-       (_, True) -> firstJust guessList (\(c, d) -> solve' $ insert c [d] board)
-       _ -> Nothing
+solve' b =
+  case (complete, valid) of
+    (True, True) -> Just board
+    (_, True) -> firstJust guessList (\(c, d) -> solve' $ insert c [d] board)
+    _ -> Nothing
   where
+    board = prune b
+    complete = isFilled board
+    valid = isValid board
     guessList =
       concatMap (\(cell, digits) -> [(cell, x) | x <- digits]) $
       sortOn (length . snd) $ filter (\d -> length (snd d) > 1) $ toAscList board
@@ -72,12 +84,9 @@ solve' board =
       in case r of
            j@(Just _) -> j
            _          -> firstJust xs f
-
-isValid board = all (isValidSpace . map (find board)) (rows ++ cols ++ grids)
-  where
-    find board cell = fromJust $ Data.Map.lookup cell board
-    isValidSpace :: [Digit] -> Bool
-    isValidSpace = comparePairwise . sort . filter (\d -> length d == 1)
-    comparePairwise xs = and $ zipWith (/=) xs (drop 1 xs)
-
-isFilled board = all (\lst -> length lst == 1) $ map snd $ toAscList board
+    isValid board = all (isValidSpace . map (find board)) (rows ++ cols ++ grids)
+      where
+        isValidSpace :: [Digit] -> Bool
+        isValidSpace = comparePairwise . sort . filter (\d -> length d == 1)
+        comparePairwise xs = and $ zipWith (/=) xs (drop 1 xs)
+    isFilled board = all (\lst -> length lst == 1) $ map snd $ toAscList board
